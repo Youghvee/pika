7f2e278c3e172208818fa9e721d9c888d7d6ed23
Ryn Lulzsec <152095647+Youghvee@users.noreply.github.com>
2024-11-13 22:53:20 +0700

    Create config.py

---
+++ Pikachu/config.py
@@ -0,0 +1,29 @@
+import os
+
+DEVS = [
+    9
+]
+
+API_ID = int(os.getenv("API_ID", ""))
+
+API_HASH = os.getenv("API_HASH", "")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+OWNER_ID = int(os.getenv("OWNER_ID", ""))
+
+USER_ID = list(map(int,os.getenv("USER_ID", "",).split(),))
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", ""))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "").split()))
+
+MAX_BOT = int(os.getenv("MAX_BOT", "30"))
+
+COMMAND = os.getenv("COMMAND", ".")
+PREFIX = COMMAND.split()
+
+MONGO_URL = os.getenv(
+    "MONGO_URL",
+    "",
+)
